8e9fa8f045a93c73f007893ffe3f1caa434f9440
David Gilson <davgilson@live.fr>
2022-02-21 15:31:05 +0100

    remove some logs

--- src/component.ts
+++ src/component.ts
@@ -4,12 +4,10 @@ import { executeCommand } from './utils';
 export function ngComponent() {
     return vscode.commands.registerCommand('ng-afelio.component', async (currentElement) => {
         // The code you place here will be executed every time your command is executed
-        console.log(currentElement.path);
         const name = await vscode.window.showInputBox({ prompt: 'Name' });
         if (name === undefined) {
             return;
         }
-        console.log(name);
         const execution = executeCommand(
             currentElement.path,
             `npx ng g ng-afelio:component ${name}`,
