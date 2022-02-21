89a68042cd6f999c238dadb985937faa2f0b8d70
David Gilson <davgilson@live.fr>
2022-02-21 15:04:32 +0100

    add fix on check i18n files

--- src/extension.ts
+++ src/extension.ts
@@ -288,16 +288,54 @@ export function activate(context: vscode.ExtensionContext) {
 	disposables.push(vscode.commands.registerCommand('ng-afelio.check-i18n', async (currentElement) => {
 		let path: string = currentElement.path;
 		const isFile = path.match(/\/([a-z0-9-]+.json)$/);
-		let mainFile;
+		let mainFile: string;
 		if (isFile) {
 			mainFile = isFile[1];
 			path = path.replace(mainFile, '');
 		}
-		const execution = executeCommandAndShowResult(
-			path,
-			`npx ng-afelio check i18n ${mainFile ? `-m ${mainFile}` : ''}`
-		);
-		vscode.window.withProgress({ location: vscode.ProgressLocation.Window, title: 'ng-afelio processing' }, () => execution );
+
+		const options: vscode.QuickPickItem[] = [
+			{ label: 'fix', description: "Align other files" }
+		];
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.canSelectMany = true;
+		quickPick.items = options;
+
+		quickPick.onDidAccept(() => {
+			let isFix = false;
+			const selectedOptions: string = options.reduce((result: string, item: vscode.QuickPickItem) => {
+				const isSelected = quickPick.selectedItems.includes(item);
+				if (isSelected) {
+					if (item.label === 'fix') {
+						isFix = true;
+					}
+					return `${result} --${item.label}`;
+				} else {
+					return result;
+				}
+			}, '');
+
+			const command = `npx ng-afelio check i18n ${mainFile ? `-m ${mainFile}` : ''} ${selectedOptions}`;
+			quickPick.hide();
+
+			let execution: Promise<void>;
+			if (isFix) {
+				execution = executeCommand(
+					path,
+					command,
+					'I18n files aligned',
+					'Can not align i18n files'
+				);
+			} else {
+				execution = executeCommandAndShowResult(
+					path,
+					command
+				);
+			}
+			vscode.window.withProgress({ location: vscode.ProgressLocation.Window, title: 'ng-afelio processing' }, () => execution );
+		});
+		
+		quickPick.show();
 	}));
 
 	disposables.push(vscode.commands.registerCommand('ng-afelio.i18n', async (currentElement) => {
